fd93c375f85f8de39b6596bc78072e2977b50860
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2022-11-20 18:09:44 -0500

    Update Happynumber.js

--- Happynumber.js
+++ Happynumber.js
@@ -36,3 +36,14 @@ var isHappy = function(n) {
         }
     }
 };
+
+
+/* The toString() method returns a string as a string. The toString() method does not change the original string. 
+The toString() method can be used to convert a string object into a string. 
+The split() method splits a string into an array of substrings. The split() method returns the new array. 
+The split() method does not change the original string. If (" ") is used as separator, the string is split between words.
+map() creates a new array from calling a function for every array element. map() calls a function once for each element in an array.
+map() does not execute the function for empty elements.
+The reduce() method executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return 
+value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
+*/
