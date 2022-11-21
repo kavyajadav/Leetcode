08664115ab7cc0cdcbb06287b01cb7c79f11493a
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2022-11-20 21:16:02 -0500

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,13 @@
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+*/
+
+var isAnagram = function(s, t) {
+    var a = s.split("").sort();
+    var b = t.split("").sort();
+    return a.length === b.length && a.every((val, index) => val === b[index]);
+};
